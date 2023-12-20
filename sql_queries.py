5c7780eb65473475468e654acb99e562800fcaa1
justejakstaite <juste.jakstaite93@gmail.com>
2023-12-20 14:35:01 +0200

    fix: update lists for queries

--- sql_queries.py
+++ sql_queries.py
@@ -61,7 +61,7 @@ songplay_table_create = ("""
      CREATE TABLE IF NOT EXISTS songplays
     (
         songplay_id    BIGINT IDENTITY(1, 1) PRIMARY KEY,
-        start_time     TIMESTAMP NOT NULL SORTKEY,
+        start_time     TIMESTAMP NOT NULL SORTKEY DISTKEY,
         user_id        TEXT NOT NULL DISTKEY,
         level          TEXT,
         song_id        TEXT,
@@ -212,7 +212,10 @@ time_table_insert = ("""
 
 # QUERY LISTS
 
-create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
-drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create,
+                        user_table_create, song_table_create, artist_table_create, time_table_create]
+drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop,
+                      song_table_drop, artist_table_drop, time_table_drop]
 copy_table_queries = [staging_events_copy, staging_songs_copy]
-insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
+insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert,
+                        time_table_insert]
